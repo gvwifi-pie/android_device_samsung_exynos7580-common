fa4cc5b432bd879ffbabf04a96bccab9557568e0
lr7875 <jeremychucy@outlook.com>
2026-08-03 20:04:24 +0800

    Use new slsi

--- device-common.mk
+++ device-common.mk
@@ -21,5 +21,5 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 -include $(LOCAL_PATH)/product/*.mk
 
 # call Samsung LSI board support package
-$(call inherit-product, hardware/samsung_slsi-cm/exynos5/exynos5.mk)
-$(call inherit-product, hardware/samsung_slsi-cm/exynos7580/exynos7580.mk)
+$(call inherit-product, hardware/samsung_slsi/exynos5/exynos5.mk)
+$(call inherit-product, hardware/samsung_slsi/exynos7580/exynos7580.mk)
